fb2f7c99c6fe704fa648c215963acb4bca3d37f4
Edgar De Los Santos <eddlsh112003@gmail.com>
2024-11-16 10:34:40 -0600

    Hola este es el primer commit

---
+++ bbva.java
@@ -0,0 +1,13 @@
+class Persona{
+    String nombre;
+    String Sexo;
+    int edad;
+}
+
+    Persona(String nombre, String sexo, int edad)
+
+class Programa {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
