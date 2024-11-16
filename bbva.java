bfee21f974907218fb5752575c5dc764b16ec3d9
Edgar De Los Santos <eddlsh112003@gmail.com>
2024-11-16 10:46:55 -0600

    segunda parte

--- bbva.java
+++ bbva.java
@@ -1,10 +1,15 @@
 class Persona{
     String nombre;
-    String Sexo;
+    String sexo;
     int edad;
-}
 
-    Persona(String nombre, String sexo, int edad)
+
+    Persona(String nombre, String sexo, int edad){
+        this.nombre = nombre;
+        this.sexo = sexo;
+        this.edad = edad;
+    }
+}
 
 class Programa {
     public static void main(String[] args) {
